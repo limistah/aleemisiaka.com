3f317f3bdc1330af5c3c864ccd82418e5e9f75ca
Aleem Isiaka <aleemisiaka@gmail.com>
2019-12-14 08:07:33 +0100

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `Self Taught Co.de`,
+    author: `Aleem Isika`,
+    description: `My site description...`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/limistah`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/limistah`,
+      },
+    ],
+  },
+}

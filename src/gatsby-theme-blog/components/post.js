e2b61d42b3190a97e08690061f422cd701787e1c
Aleem Isiaka <aleemisiaka@gmail.com>
2019-12-17 21:48:41 +0100

    Other users can make contributions

--- src/gatsby-theme-blog/components/post.js
+++ src/gatsby-theme-blog/components/post.js
@@ -16,8 +16,11 @@ const Post = ({
   location,
   previous,
   next,
-}) => (
-  <Layout location={location} title={title}>
+}) => {
+  let filename = post.slug.split("/");
+  filename = filename[2];
+  return (
+    <Layout location={location} title={title}>
     <SEO title={post.title} description={post.excerpt} />
     <main>
       <Styled.h1>{post.title}</Styled.h1>
@@ -33,9 +36,29 @@ const Post = ({
 
       <Styled.hr />
       <MDXRenderer>{post.body}</MDXRenderer>
+
+      <Styled.a
+        css={css({ my: 2 })}
+        href={`https://github.com/limistah/aleemisiaka.com/blob/master/content/posts/${filename}.mdx`}
+      >
+        <svg
+          css={css({ verticalAlign: "middle", mr: 1 })}
+          fill="currentColor"
+          preserveAspectRatio="xMidYMid meet"
+          height="1em"
+          width="1em"
+          viewBox="0 0 40 40"
+        >
+          <g>
+            <path d="m34.5 11.7l-3 3.1-6.3-6.3 3.1-3q0.5-0.5 1.2-0.5t1.1 0.5l3.9 3.9q0.5 0.4 0.5 1.1t-0.5 1.2z m-29.5 17.1l18.4-18.5 6.3 6.3-18.4 18.4h-6.3v-6.2z"></path>
+          </g>
+        </svg>
+        Edit on github
+      </Styled.a>
     </main>
     <PostFooter {...{ previous, next }} />
   </Layout>
-)
+  )
+}
 
 export default Post

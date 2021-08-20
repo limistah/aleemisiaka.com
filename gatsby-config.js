ff0bc0cf2d8d594cbcd83c0177a9ea93d60c9a2d
Aleem Isiaka <aleemisiaka@gmail.com>
2021-08-20 21:54:23 +0100

    Add notes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: "gatsby-starter-code-notes",
+    showThemeInfo: false,
+    title: "AI's Notes",
     description: `Limistah's Notes`,
-    author: "AI",
+    author: "Aleem Isiaka",
   },
   plugins: [
     {
@@ -14,15 +15,14 @@ module.exports = {
         showDescriptionInSidebar: true,
         showDate: true,
         gitRepoContentPath:
-          "https://github.com/mrmartineau/gatsby-starter-code-notes/tree/master/notes",
-        // openSearch: {
-        //   siteShortName: `My Code Notes`,
-        //   siteUrl: 'https://my-website.com',
-        //   siteTags: 'code',
-        //   siteContact: 'https://twitter.com/???',
-        //   siteDescription:
-        //     'Code Notes',
-        // },
+          "https://github.com/limistah/aleemisiaka.com/tree/notes/notes",
+        openSearch: {
+          siteShortName: `AI's Notes`,
+          siteUrl: "https://notes.aleemisiaka.com",
+          siteTags: "code, writing, code, ideas",
+          siteContact: "https://twitter.com/limisth",
+          siteDescription: "Aleem Isiaka's Notes",
+        },
       },
     },
   ],

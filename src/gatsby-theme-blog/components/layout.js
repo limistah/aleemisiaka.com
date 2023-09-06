a71312275b7de41225acf93162e3da3ac32d7dcc
Aleem Isiaka <aleemisiaka@gmail.com>
2023-09-06 10:29:10 +0100

    Layout

--- src/gatsby-theme-blog/components/layout.js
+++ src/gatsby-theme-blog/components/layout.js
@@ -3,30 +3,28 @@ import { css, Styled } from "theme-ui"
 import Header from "./header"
 import Footer from "./footer"
 import SEO from "../components/seo"
-import { Router } from "@gatsbyjs/reach-router"
+import { LocationContext } from "@gatsbyjs/reach-router"
 
 export default ({ children, ...props }) => (
-  <Router>
-    <Styled.root>
-      <SEO title={props.seo?.title} description={props.seo?.description} />
+  <Styled.root>
+    <SEO title={props.seo?.title} description={props.seo?.description} />
 
-      <Header {...props} />
-      <Styled.hr css={css({ m: 0 })} />
+    <Header {...props} />
+    <Styled.hr css={css({ m: 0 })} />
 
-      <div>
-        <div
-          css={css({
-            maxWidth: `container`,
-            mx: `auto`,
-            px: 2,
-            py: 4,
-          })}
-        >
-          {children}
-        </div>
+    <div>
+      <div
+        css={css({
+          maxWidth: `container`,
+          mx: `auto`,
+          px: 2,
+          py: 4,
+        })}
+      >
+        {children}
       </div>
-      <Styled.hr css={css({ m: 0 })} />
-      <Footer />
-    </Styled.root>
-  </Router>
+    </div>
+    <Styled.hr css={css({ m: 0 })} />
+    <Footer />
+  </Styled.root>
 )

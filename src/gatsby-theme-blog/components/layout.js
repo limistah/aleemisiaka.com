6726b5764860a34dbeb7247ffcf64d0c0d89567d
Aleem Isiaka <aleemisiaka@gmail.com>
2023-09-06 10:15:55 +0100

    Use Router

--- src/gatsby-theme-blog/components/layout.js
+++ src/gatsby-theme-blog/components/layout.js
@@ -3,27 +3,30 @@ import { css, Styled } from "theme-ui"
 import Header from "./header"
 import Footer from "./footer"
 import SEO from "../components/seo"
+import { Router } from "@gatsbyjs/reach-router"
 
 export default ({ children, ...props }) => (
-  <Styled.root>
-    <SEO title={props.seo?.title} description={props.seo?.description} />
+  <Router>
+    <Styled.root>
+      <SEO title={props.seo?.title} description={props.seo?.description} />
 
-    <Header {...props} />
-    <Styled.hr css={css({ m: 0 })} />
+      <Header {...props} />
+      <Styled.hr css={css({ m: 0 })} />
 
-    <div>
-      <div
-        css={css({
-          maxWidth: `container`,
-          mx: `auto`,
-          px: 2,
-          py: 4,
-        })}
-      >
-        {children}
+      <div>
+        <div
+          css={css({
+            maxWidth: `container`,
+            mx: `auto`,
+            px: 2,
+            py: 4,
+          })}
+        >
+          {children}
+        </div>
       </div>
-    </div>
-    <Styled.hr css={css({ m: 0 })} />
-    <Footer />
-  </Styled.root>
+      <Styled.hr css={css({ m: 0 })} />
+      <Footer />
+    </Styled.root>
+  </Router>
 )
